a34bcc4d6c357bd833bf8be605f40b65f10b54fa
caosaje1 <caosaje1@gmail.com>
2017-05-01 22:46:16 -0500

    PHP

---
+++ app/src/main/java/com/bernardo/agendasql/Config.java
@@ -0,0 +1,13 @@
+package com.bernardo.agendasql;
+
+/**
+ * Created by BERNARDO on 1/05/2017.
+ */
+
+public class Config {
+    public static final String URL_ADD="http://192.168.1.9/agenda/create_contact.php";
+    public static final String URL_UPDATE="http://192.168.1.9/agenda/update_contact.php";
+    public static final String URL_DELETE="http://192.168.1.9/agenda/delete_contact.php?nombre=";
+    public static final String URL_GET_CONTACT="http://192.168.1.9/agenda/get_contact_details.php?nombre=";
+}
+
--- app/src/main/java/com/bernardo/agendasql/MainActivity.java
+++ app/src/main/java/com/bernardo/agendasql/MainActivity.java
@@ -1,91 +1,216 @@
 package com.bernardo.agendasql;
 
+import android.app.ProgressDialog;
 import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import java.util.HashMap;
+import static com.bernardo.agendasql.R.id.eCorreo;
+import static com.bernardo.agendasql.R.id.eNombre;
 
 public class MainActivity extends AppCompatActivity {
 
+ 
 
-    EditText eNombre, eCorreo, eTelefono;
-    String nombre, correo, telefono;
-    ContentValues dataBD;
+        EditText eNombre, eCorreo, eTelefono;
+        String nombre, correo, telefono;
 
-    //Button bGuardar, bBuscar, bEliminar, bActualiza;*/
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
+            eNombre = (EditText) findViewById(R.id.eNombre);
+            eCorreo = (EditText) findViewById(R.id.eCorreo);
+            eTelefono = (EditText) findViewById(R.id.eTelefono);
 
-    ContactosSQLiteHelper contactosSQLiteHelper;
-    SQLiteDatabase dbContactos;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        contactosSQLiteHelper = new ContactosSQLiteHelper(this, "ContactosDB", null,1);
-        dbContactos = contactosSQLiteHelper.getWritableDatabase();
-
-        eNombre = (EditText) findViewById(R.id.eNombre);
-        eCorreo = (EditText) findViewById(R.id.eCorreo);
-        eTelefono = (EditText) findViewById(R.id.eTelefono);
-        /*bGuardar = (Button) findViewById(R.id.bGuardar);
-        bBuscar = (Button) findViewById(R.id.bBuscar);
-        bGuardar = (Button) findViewById(R.id.bGuardar);
-        bGuardar = (Button) findViewById(R.id.bGuardar);*/
-
-
-    }
+        };
 
     public void onClick(View view) {
-        int id=view.getId();
+        int id = view.getId();
 
-        nombre=eNombre.getText().toString();
-        correo=eCorreo.getText().toString();
-        telefono=eTelefono.getText().toString();
+        nombre = eNombre.getText().toString();
+        correo = eCorreo.getText().toString();
+        telefono = eTelefono.getText().toString();
 
-
-        dataBD = new ContentValues();
-
-
-        switch (id){
+        switch(id){
             case R.id.bGuardar:
-                dataBD.put("nombre",nombre);
-                dataBD.put("correo",correo);
-                dataBD.put("telefono",telefono);
-                dbContactos.insert("Contactos",null,dataBD);
-
-                dbContactos.execSQL("INSERT INTO Contactos VALUES (null, '"+nombre+"', '"+telefono+"', '"+correo+"')"); //sintaxis sql
-
+                addContact();
+                limpiar();
                 break;
             case R.id.bBuscar:
-                Cursor cursor= dbContactos.rawQuery("SELECT * FROM Contactos WHERE nombre='"+nombre+"'",null);
-
-                if (cursor.moveToFirst()){
-                    eTelefono.setText(cursor.getString(2));
-                    eCorreo.setText(cursor.getString(3));
-                }
-
+                showContact();
                 break;
             case R.id.bModificar:
-                dataBD.put("correo",correo);
-                dataBD.put("telefono",telefono);
-                dbContactos.update("Contactos",dataBD,"nombre'"+nombre+"'",null);
-
-                dbContactos.execSQL("UPDATE contactos SET telefono='"+telefono+"', correo='"+correo+"' WHERE nombre='"+nombre+"'"); //sinTaxis sql
-
+                updateContact();
+                limpiar();
                 break;
             case R.id.bEliminar:
-                dbContactos.delete("Contactos","nombre'"+nombre+"'",null);
-
-                dbContactos.execSQL("DELETE FROM Contactos WHERE nombre='"+nombre+"'"); // sintaxis sqlite
-
+                deleteContact();
+                limpiar();
                 break;
         }
     }
+
+    private void showContact() {
+        class ShowContact extends AsyncTask<Void, Void, String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Show...","Wait...",false,false);
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendGetRequestParam(Config.URL_GET_CONTACT, nombre);
+                return res;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                showData(s);
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        }
+        ShowContact ae = new ShowContact();
+        ae.execute();
+    }
+
+    private void showData(String json) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray result = jsonObject.getJSONArray("result");
+            JSONObject c = result.getJSONObject(0);
+            String telefonoJ = c.getString("telefono");
+            String correo = c.getString("correo");
+            eTelefono.setText(telefonoJ);
+            eCorreo.setText(correo);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void deleteContact() {
+        class DeleteContact extends AsyncTask<Void, Void, String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Delete...","Wait...",false,false);
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendGetRequestParam(Config.URL_DELETE, nombre);
+                return res;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        }
+        DeleteContact ae = new DeleteContact();
+        ae.execute();
+    }
+
+    private void updateContact() {
+        class UpdateContact extends AsyncTask<Void, Void, String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Updating...","Wait...",false,false);
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("nombre",nombre);
+                params.put("telefono",telefono);
+                params.put("correo",correo);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Config.URL_UPDATE, params);
+                return res;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        }
+        UpdateContact ae = new UpdateContact();
+        ae.execute();
+    }
+
+    private void limpiar() {
+        eNombre.setText("");
+        eCorreo.setText("");
+        eTelefono.setText("");
+    }
+
+    private void addContact() {
+        class AddContact extends AsyncTask<Void, Void, String>{
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this,"Adding...","Wait...",false,false);
+            }
+
+            @Override
+            protected String doInBackground(Void... v) {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("nombre",nombre);
+                params.put("telefono",telefono);
+                params.put("correo",correo);
+
+                RequestHandler rh = new RequestHandler();
+                String res = rh.sendPostRequest(Config.URL_ADD, params);
+                return res;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+        }
+        AddContact ae = new AddContact();
+        ae.execute();
+    }
+
 }
